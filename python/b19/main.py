12af4f8c4f9e8110c6e834dd39b9c0ba37f8ac24
8 Sync Dev - Anh Tu Dev <tuan8165@gmail.com>
2025-08-21 11:15:39 +0700

    8syncdev

---
+++ python/b19/main.py
@@ -0,0 +1,15 @@
+import numpy
+
+list1d = [1, 2, 3, 4, 5]
+arr1d = numpy.array(list1d)
+
+print(arr1d)
+
+list2d = [
+    [1,2,3],
+    [4,5,6]
+]
+
+arr2d = numpy.array(list2d)
+# print(list2d)
+print(arr2d)
